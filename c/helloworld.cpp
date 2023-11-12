ec9ebc1803e28f4ddc277096b0b53381f29dc9d9
elessards <elessards@gmail.com>
2023-11-11 18:30:29 -0800

    first cpp code

---
+++ c/helloworld.cpp
@@ -0,0 +1,10 @@
+// Author: Justin Pedersen
+// Date: 11/11/23
+// Description: first C++ code; basic hello world
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!";
+    return 0;
+}
